4b33ed209e2a1970b4b1ba037b4026e571cdeb4a
nikukeke <waiwaiguoke@sina.com>
2020-08-04 22:59:04 +0800

    console

--- week17/toolchain/console/index.js
+++ week17/toolchain/console/index.js
@@ -44,6 +44,13 @@
 
 
 var stdin = process.stdin;
+var tty = require('tty');
+var ttys = require('ttys');
+var rl = require('readline'); 
+
+var stdin = ttys.stdin;
+var stdout = ttys.stdout;
+
 // without this, we would only get streams once enter is pressed
 stdin.setRawMode( true );
 
@@ -65,23 +72,75 @@ stdin.setEncoding( 'utf8' );
  
 function getChar(){
     return new Promise(resolve => {
-        stdin.on('data', function (key) {
+        stdin.once('data', function (key) {
             resolve(key);
         })
     })
 }
 
+function up(n = 1){
+    stdout.write('\033['+n+'A');
+}
+
+function down(n = 1){
+    stdout.write('\033['+n+'B');
+}
+
+function right(n = 1){
+    stdout.write('\033['+n+'C');
+}
+
+function left(n = 1){
+    stdout.write('\033['+n+'D');
+}
+
 void async function() {
+    stdout.write('Which framework do you want to use?\n');
+    let answer = await select(["vue","react","angular"]); 
+    stdout.write('You selected ' + answer + "!!!\n");
+    process.exit();
+}()
+
+async function select(choices) {
+    let selected = 0;
+    for(let i = 0 ; i <choices.length; i++) { 
+        let choice = choices[i];
+        if(i === selected) {
+            stdout.write("[\x1B[32mx\x1B[0m] " + choice + "\n");
+        } else {
+            stdout.write("[ ] " + choice + "\n");
+        } 
+    }
+
+    up(choices.length);
+    right()
     while(true) {
-        let char = await getChar();
-        // console.log(char.charCodeAt(0));
-        if(char === "\u0003")
-        {
+        let char = await getChar(); 
+        if(char === "\u0003"){
             process.exit();
             break;
+        } 
+        if(char === "w" && selected > 0) {
+            stdout.write(" ");
+            left()
+            selected--;
+            up();
+            stdout.write("\x1B[32mx\x1B[0m");
+            left()
+        } 
+        if(char === "s" && selected < choices.length - 1) {
+            stdout.write(" ");
+            left()
+            selected++;
+            down();
+            stdout.write("\x1B[32mx\x1B[0m");
+            left() 
         }
-            
-        console.log(char.split('').map(c => c.charCodeAt(0)));
-        // if(await getChar() === )
+        if(char === "\r") {
+
+            down(choices.length - selected);
+            left() 
+            return choices[selected];
+        }
     }
-}()
+}
---
+++ week17/week17.md
@@ -0,0 +1,16 @@
+
+
+yeoman 
+
+https://yeoman.io/authoring/file-system.html
+
+
+
+node.js
+
+https://nodejs.org/docs/latest-v13.x/api/readline.html
+
+https://stackoverflow.com/questions/5006821/nodejs-how-to-read-keystrokes-from-stdin
+
+
+
