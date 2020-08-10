ea3b09fb40384d7b1d418a691c51579aad722ec1
nikukeke <waiwaiguoke@sina.com>
2020-08-10 23:59:25 +0800

    html-parser test

---
+++ week18/html-parser/test/parser.test.js
@@ -0,0 +1,8 @@
+// let mod = require("../src/add.js");
+import { parseHTML } from "../src/parser.js"
+let assert = require('assert');
+
+it("should add to numbers from an es module", () => {
+    // assert.equal(mod.add(3, 5), 8);
+    parseHTML("<div></div>")
+})
